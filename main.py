da1a70bc5b2483216b2f84164e56e1b83ee69f78
DarkWerb22 <davydivdeniska@gmail.com>
2025-02-28 08:59:04 +0200

    pep8

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from string import printable
 from random import choice
+
+
 symbols = list(printable[:-6])
 status = False
 while True:
     length = int(input("Выберете длинну пароля от 7 до 20: "))
-    if length <7 or length >20:
+    if length < 7 or length > 20:
         print("Пароль слишком длинный, Попробуйте еще раз")
     else:
         break
@@ -21,5 +23,3 @@ while not status:
     else:
         print("Система не понимает вашего ответа")
         break
-
-
